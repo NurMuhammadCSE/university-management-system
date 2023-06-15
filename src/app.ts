83a1d21de6f6e8b81ffa64dd5ca2bda4789d000a
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2023-06-15 22:54:32 +0600

    Error Handling

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-import { Application, NextFunction, Request, Response } 
-from "express"
-import express from "express"
-import cors from "cors"
+import { Application, NextFunction, Request, Response } from 'express'
+import express from 'express'
+import cors from 'cors'
 import usersRouter from './app/modules/user/user.route'
-import userService from "./app/modules/user/user.service"
+// import userService from "./app/modules/user/user.service"
 
-const app:Application = express()
+const app: Application = express()
 
 app.use(cors())
 app.use(express.json())
@@ -13,8 +12,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1/users/', usersRouter)
 
-
-app.get('/', async(req: Request, res:Response, next: NextFunction) => {
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
   // await userService.createUser({
   //   id: '111',
   //   password: "password",
