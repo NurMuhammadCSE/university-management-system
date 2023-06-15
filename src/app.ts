aa5445c153d252b1e78be4748ba3f98ea8ff7443
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2023-06-15 22:44:17 +0600

    Basic User Setup

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import { Application, NextFunction, Request, Response } 
+from "express"
+import express from "express"
+import cors from "cors"
+import usersRouter from './app/modules/user/user.route'
+import userService from "./app/modules/user/user.service"
+
+const app:Application = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/v1/users/', usersRouter)
+
+
+app.get('/', async(req: Request, res:Response, next: NextFunction) => {
+  // await userService.createUser({
+  //   id: '111',
+  //   password: "password",
+  //   role: 'admin',
+  // })
+  res.send('Working Successfully')
+  next()
+})
+
+export default app
